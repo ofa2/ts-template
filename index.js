9628834a2b1bb480faccac87a330daac063ac5db
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-25 07:22:58 +0000

    Deploy ofa2/ts-template to github.com/ofa2/ts-template.git:npm

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = lift;
+
+function lift(config) {
+  // eslint-disable-next-line no-console
+  console.log(config);
+}
+//# sourceMappingURL=index.js.map
